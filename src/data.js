60dad07a08a006566bfc679f2c1b6408f6f2ddab
Paula_Estay <paulaestayc@gmail.com>
2019-05-16 00:16:40 -0400

    Muestra Filtra y Calcula Data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,34 @@
+const filterLabel = (data, condition)=>{
+	const resultFilter = data.filter(element =>{
+		return element.feedlabel.includes(condition);
+	}) 
+	return resultFilter;
+}
+
+window.filterLabel=filterLabel;
+
+
+const computeStats = (arr) => {
+	let result = arr.length
+	return result;
+}
+window.computeStats=computeStats
+
+
+const sortData = (data, condition2) => {
+	
+	let orderData = dataSteam.sort((a,b) => (a[condition2] > b[condition2]) ? 1 : -1);
+	return orderData;
+	console.log(orderData)
+}
+window.sortData = sortData;
+
+//CALCULAR PORCENTAJE
+// const computeStats = (arr, data) => {
+//   let result = Math.round((arr.length * 100) / data.length);
+//     return result;
+// }
+// window.computeStats=computeStats
 /* Manejo de data */
 
 // esta es una función de ejemplo
--- src/main.js
+++ src/main.js
@@ -1,1 +1,72 @@
 /* Manejo del DOM */
+
+let dataSteam = STEAM.appnews.newsitems;
+let container = document.getElementById("feedlabel")
+let count = document.getElementById("count")
+const selectOrderdata = document.getElementById("orderdata");
+const showAll = document.getElementById("root");
+
+
+const createCard =(data)=>{
+  showAll.innerHTML='';
+  data.forEach(component => {
+    if (component.author === '' && component.feedlabel !== '') {
+      showAll.innerHTML += `
+      <div>
+      <div class="container col-sm-6 col-md-10 my-3">
+      <div class="card text-#aaa bg-light mb-3 ">
+      <div class="box">       
+      <div class="ahref"><a href="${component.url}"></div>        
+      <h3>${component.title}</h3>
+      <div class="text">
+      <h5>Autor: Desconocido </h5>        
+      <p>${component.contents}</p>
+      <p>FeedLabel: ${component.feedlabel}</p>
+      </div>
+      </div>
+      </div>
+      </div>`
+    } 
+    else {
+      showAll.innerHTML += `
+      <div>
+      <div class="container col-sm-6 col-md-10 my-3">
+      <div class="card text-#aaa bg-light mb-3 ">
+      <div class="box">        
+      <div class="ahref">
+      <a href="${component.url}"></div>        
+      <h3>${component.title}</h3>
+      <div class="text">
+      <h5>Autor:${component.author}</h5>        
+      <p>${component.contents}</p>
+      <p>FeedLabel: ${component.feedlabel}</p>
+      </div>
+      </div>
+      </div>
+      </div>` 
+    }
+  });
+}
+createCard(dataSteam)
+
+//FILTRAMOS POR FUENTE
+const selectSrc = document.getElementById("feedlabel");
+selectSrc.addEventListener("change", () =>{
+  let condition= selectSrc.options[selectSrc.selectedIndex].text;
+  let filterLabel=window.filterLabel(dataSteam,condition);
+  count.innerHTML="Se encontraron " + window.computeStats(filterLabel,dataSteam) + " noticias provenientes de " + condition;
+  filterLabel.forEach (() =>{
+    return createCard(filterLabel)
+  })
+})  
+
+
+selectOrderdata.addEventListener('change', () => {
+ let condition2 = selectOrderdata.value
+ let ordered = window.sortData(dataSteam, condition2);
+})
+
+document.getElementById("reset").addEventListener("click", () =>{
+  location.reload();
+})
+/* Manejo del DOM */
